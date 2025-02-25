3bc9ebadecc95f59a5dec08fa8f3e330974101c1
Pohe1999 <mapogu99@gmail.com>
2025-02-25 11:26:54 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { stringify } = require('postcss');
 require('dotenv').config();
 
 const app = express();
